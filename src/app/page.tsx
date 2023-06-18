6ac6e96f7adef12058da183a196560b0349d4150
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-06-18 20:02:59 +0700

    fix: revise HowItWorks section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import type { Metadata } from "next";
 
 import { Hero } from "./(Home)/Hero";
@@ -5,6 +6,10 @@ import { LatarBelakang } from "./(Home)/LatarBelakang";
 import { SolusiKami } from "./(Home)/SolusiKami";
 
 import { EmtekaIcon } from "./(icons)/Emteka";
+import FlowerSmall1 from "@images/home/flower_small_1.png";
+import FlowerSmall2 from "@images/home/flower_small_2.png";
+import FlowerLarge1 from "@images/home/flower_large_1.png";
+import FlowerLarge2 from "@images/home/flower_large_2.png";
 
 export const metadata: Metadata = {
   title: "Emteka",
@@ -33,30 +38,57 @@ export default function Home() {
       <LatarBelakang />
       <SolusiKami />
 
-      <section className="bg-secondary-50 mt-9 py-10 px-6 desktop:mt-20 desktop:px-20 desktop:py-20 mediumDesktop:px-32">
-        <h2 className="text-neutral-900 font-semibold text-center text-2xl mb-4 desktop:text-4xl desktop:mb-12">
+      <div className="bg-secondary-50 mt-14 py-6 px-8 lg:py-8 xl:py-10">
+        <h2 className="text-neutral-900 font-semibold text-center text-2xl mb-6 lg:text-[32px] lg:mb-8">
           How It Works
         </h2>
 
-        <iframe
-          allowFullScreen
-          src="https://www.youtube.com/embed/_JTp4JzZHbY"
-          title="Cara Kerja Emteka"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          className="rounded-xl w-full h-[200px] desktop:w-[868px] desktop:h-[404px] desktop:mx-auto"
-        ></iframe>
-      </section>
+        <div className="relative w-full overflow-hidden pt-[50%] lg:w-[868px] lg:h-[404px] lg:pt-0 lg:mx-auto">
+          <iframe
+            allowFullScreen
+            src="https://www.youtube.com/embed/_JTp4JzZHbY"
+            title="Cara Kerja Emteka"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            className="absolute top-0 bottom-0 right-0 left-0 rounded-lg w-full h-full border-none"
+          ></iframe>
+        </div>
+      </div>
 
-      <section className="bg-secondary-500 text-center flex flex-col justify-center items-center rounded-xl mt-9 mx-6 h-[200px] desktop:mt-20 desktop:w-[868px] desktop:h-[448px] desktop:mx-auto">
-        <h4 className="text-neutral-0 font-semibold text-[32px] leading-[38px] w-10/12 mx-auto mb-2 desktop:font-extrabold desktop:text-[88px] desktop:leading-normal desktop:mb-2">
-          Coming Soon
-        </h4>
+      <div className="mt-14 mx-8">
+        <div className="relative w-full overflow-hidden pt-[50%] lg:w-[868px] lg:h-[404px] lg:pt-0 lg:mx-auto">
+          <div className="bg-secondary-500 absolute top-0 bottom-0 right-0 left-0 flex flex-col justify-center items-center rounded-lg w-full h-full">
+            <Image
+              src={FlowerSmall1}
+              alt=""
+              className="absolute top-0 left-0 lg:hidden"
+            />
+            <Image
+              src={FlowerSmall2}
+              alt=""
+              className="absolute bottom-0 right-0 lg:hidden"
+            />
+            <Image
+              src={FlowerLarge1}
+              alt=""
+              className="hidden lg:block lg:absolute lg:top-0 lg:left-0"
+            />
+            <Image
+              src={FlowerLarge2}
+              alt=""
+              className="hidden lg:block lg:absolute lg:bottom-0 lg:right-0"
+            />
 
-        <p className="text-neutral-0 font-medium text-xs w-10/12 mx-auto desktop:text-xl">
-          Segera hadir! Tetap terhubung dan jadilah yang pertama merasakan
-          kemajuan luar biasa!
-        </p>
-      </section>
+            <h3 className="text-neutral-0 font-extrabold uppercase text-[32px] leading-9 mb-2 lg:text-5xl lg:mb-4">
+              Coming Soon
+            </h3>
+
+            <p className="text-neutral-100 text-center text-xs mx-8 lg:text-base lg:w-6/12 lg:mx-auto">
+              Segera hadir! Tetap terhubung dan jadilah yang pertama merasakan
+              kemajuan luar biasa!
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
