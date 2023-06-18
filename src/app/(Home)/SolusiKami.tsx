66ce29a0a8252b90a224bc569941b7266c4f41cc
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-06-18 19:56:44 +0700

    fix: revise SolusiKami section

---
+++ src/app/(Home)/SolusiKami.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+
+import BranchingImage from "@images/home/branching.png";
+import FeedbackImage from "@images/home/feedback.png";
+import TrackingImage from "@images/home/tracking.png";
+
+export function SolusiKami() {
+  return (
+    <div className="mt-14 mx-8 lg:w-[868px] lg:mx-auto">
+      <article className="mb-8">
+        <h2 className="text-neutral-900 font-semibold text-center text-2xl mb-4 lg:text-[32px] lg:mb-6">
+          Solusi Kami
+        </h2>
+
+        <p className="text-neutral-800 text-center text-sm lg:text-base">
+          Dalam menghadapi tantangan tersebut, Emteka hadir sebagai solusi yang
+          inovatif untuk membantu memetakan kemampuan serta strategi
+          pembelajaran yang tepat dalam meningkatkan kemampuan Matematika Anda
+        </p>
+      </article>
+
+      <div className="grid gap-y-6 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-6">
+        <article className="shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-lg py-3 px-4">
+          <Image
+            src={BranchingImage}
+            alt="Fitur Branching"
+            className="object-cover object-center w-[70px] h-[70px] mb-4 lg:w-[80px] lg:h-[80px]"
+          />
+
+          <h3 className="text-neutral-900 font-semibold mb-2 lg:text-lg">
+            Fitur Branching
+          </h3>
+
+          <p className="text-neutral-800 text-sm lg:text-base">
+            Mengarahkan Anda melalui penyesuaian konten dan tantangan dari
+            asesmen berdasarkan tingkat kemampuan Anda
+          </p>
+        </article>
+
+        <article className="shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-lg py-3 px-4">
+          <Image
+            src={TrackingImage}
+            alt="Fitur Tracking"
+            className="object-cover object-center w-[70px] h-[70px] mb-4 lg:w-[80px] lg:h-[80px]"
+          />
+
+          <h3 className="text-neutral-900 font-semibold mb-2 lg:text-lg">
+            Fitur Tracking
+          </h3>
+
+          <p className="text-neutral-800 text-sm lg:text-base">
+            Melacak dan memantau kemajuan Anda dalam mempelajari Matematika
+            selama mengerjakan asesmen
+          </p>
+        </article>
+
+        <article className="shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-lg py-3 px-4">
+          <Image
+            src={FeedbackImage}
+            alt="Fitur Feedback"
+            className="object-cover object-center w-[70px] h-[70px] mb-4 lg:w-[80px] lg:h-[80px]"
+          />
+
+          <h3 className="text-neutral-900 font-semibold mb-2 lg:text-lg">
+            Fitur Feedback
+          </h3>
+
+          <p className="text-neutral-800 text-sm lg:text-base">
+            Memberikan pemetaan menyeluruh tentang kemampuan Anda yang mencakup
+            pendangan secara terperinci
+          </p>
+        </article>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import type { Metadata } from "next";
 
 import { Hero } from "./(Home)/Hero";
 import { LatarBelakang } from "./(Home)/LatarBelakang";
+import { SolusiKami } from "./(Home)/SolusiKami";
 
 import { EmtekaIcon } from "./(icons)/Emteka";
-import BranchingImage from "@images/home/branching.png";
-import FeedbackImage from "@images/home/feedback.png";
-import TrackingImage from "@images/home/tracking.png";
 
 export const metadata: Metadata = {
   title: "Emteka",
@@ -34,73 +31,7 @@ export default function Home() {
       </article>
 
       <LatarBelakang />
-
-      <section className="text-center mt-9 mx-6 desktop:mt-20 desktop:mx-20 mediumDesktop:mx-32">
-        <article>
-          <h2 className="text-neutral-900 font-semibold text-2xl mb-4 desktop:text-4xl desktop:mb-6">
-            Solusi Kami
-          </h2>
-
-          <p className="text-neutral-900 desktop:text-lg">
-            Dalam menghadapi tantangan tersebut, Emteka hadir sebagai solusi
-            yang inovatif untuk membantu memetakan kemampuan serta strategi
-            pembelajaran yang tepat dalam meningkatkan kemampuan Matematika Anda
-          </p>
-        </article>
-      </section>
-
-      <section className="grid gap-y-8 mt-10 mx-6 desktop:grid-cols-3 desktop:gap-y-0 desktop:gap-x-6 desktop:mt-12 desktop:mx-20 mediumDesktop:mx-32">
-        <article className="shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-lg py-4 px-3">
-          <Image
-            src={BranchingImage}
-            alt="Fitur Branching"
-            className="object-cover object-center w-[140px] h-[140px] mb-4"
-          />
-
-          <h3 className="text-neutral-900 font-bold text-lg mb-4 desktop:text-xl">
-            Fitur Branching
-          </h3>
-
-          <p className="text-neutral-900 desktop:text-lg">
-            Mengarahkan Anda melalui penyesuaian konten dan tantangan dari
-            asesmen berdasarkan tingkat kemampuan Anda
-          </p>
-        </article>
-
-        <article className="shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-lg py-4 px-3">
-          <Image
-            src={TrackingImage}
-            alt="Fitur Tracking"
-            className="object-cover object-center w-[140px] h-[140px] mb-4"
-          />
-
-          <h3 className="text-neutral-900 font-bold text-lg mb-4 desktop:text-xl">
-            Fitur Tracking
-          </h3>
-
-          <p className="text-neutral-900 desktop:text-lg">
-            Melacak dan memantau kemajuan Anda dalam mempelajari Matematika
-            selama mengerjakan asesmen
-          </p>
-        </article>
-
-        <article className="shadow-[0_0_8px_rgba(0,0,0,0.25)] rounded-lg py-4 px-3">
-          <Image
-            src={FeedbackImage}
-            alt="Fitur Feedback"
-            className="object-cover object-center w-[140px] h-[140px] mb-4"
-          />
-
-          <h3 className="text-neutral-900 font-bold text-lg mb-4 desktop:text-xl">
-            Fitur Feedback
-          </h3>
-
-          <p className="text-neutral-900 desktop:text-lg">
-            Memberikan pemetaan menyeluruh tentang kemampuan Anda yang mencakup
-            pendangan secara terperinci
-          </p>
-        </article>
-      </section>
+      <SolusiKami />
 
       <section className="bg-secondary-50 mt-9 py-10 px-6 desktop:mt-20 desktop:px-20 desktop:py-20 mediumDesktop:px-32">
         <h2 className="text-neutral-900 font-semibold text-center text-2xl mb-4 desktop:text-4xl desktop:mb-12">
