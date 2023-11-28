f5a3550ec54870ab0d12aeeaa0bada6476f4507c
Wahyudi Umar <wahyucherryorlove@gmail.com>
2023-11-28 13:54:30 +0700

    Fix: Ubah tampilan button saat tampilan di zoom in out

--- src/components/whatsappButton.tsx
+++ src/components/whatsappButton.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 
 export function WhatsappButton() {
   return (
-    <Link href="https://wa.me/6282211000129" target="_blank">
-      <div className="fixed bottom-12 right-12 w-[52px] p-[6px] bg-[#4075FF80] border-4 border-[#FFFFFF80] rounded-xl z-10">
-        <WhatsAppColored />
-      </div>
-    </Link>
+    <div className="mx-auto w-[1300px] relative flex flex-col justify-between items-end bg-error-400">
+      <Link href="https://wa.me/6282211000129" target="_blank">
+        <div className="fixed bottom-12 w-[52px] p-[6px] bg-[#4075FF80] border-4 border-[#FFFFFF80] rounded-xl z-10">
+          <WhatsAppColored />
+        </div>
+      </Link>
+    </div>
   );
 }
