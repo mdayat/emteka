b82c364b64665fbd23fa4ba9c8499d48702d861c
Ethaniel <ravaneloe@gmail.com>
2023-11-29 20:25:16 +0700

    feat: Minor Change

--- src/features/home/LatarBelakang.tsx
+++ src/features/home/LatarBelakang.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Bookmark from "../../components/icons/collections_bookmark.svg";
 import GreenCheck from "../../components/icons/green_check_circle.svg";
 import Edit from "../../components/icons/edit.svg";
+import Arrow from "../../components/icons/arrow_downward.svg";
 
 import Circle from "@images/home/circle_effect.png";
 import MainImage from "@images/home/Emteka_Screen_02_1.png";
@@ -12,17 +13,20 @@ import BottomLeft from "@images/home/Emteka_Screen_02_3.png";
 import SecondMain from "@images/home/Emteka_Screen_03_1.png";
 import SecondBottom from "@images/home/Emteka_Screen_03_2.png";
 
+import Effect01 from "@images/home/Effect_01.png";
+
 import ThirdMain from "@images/home/Emteka_Screen_04.png";
 export function LatarBelakang() {
   return (
     <article className="bg-secondary-500 relative overflow-hidden font-karla">
-      <div className="w-[1000px] mx-auto mt-10">
-        <div className="flex">
+      <div className="mt-10">
+        <div className="flex w-[1000px] mx-auto ">
           <p className="text-[30px] font-semibold text-neutral-0 tracking-widest w-[60%]">
             Cara mengukur kemampuan matematika Anda
           </p>
           <div className="flex gap-x-5">
-            <button className="bg-transparent border-2 border-solid border-primary-100 text-primary-100 py-2.5 px-7 rounded-full mt-5 h-fit">
+            <button className="bg-transparent border-2 border-solid border-primary-100 text-primary-100 py-2.5 px-7 rounded-full mt-5 h-fit flex">
+              <Image src={Arrow} alt="Arrow SVG" className="mr-3" />
               Lihat Di bawah
             </button>
             <a href="https://youtu.be/_JTp4JzZHbY" target="_blank">
@@ -33,7 +37,7 @@ export function LatarBelakang() {
           </div>
         </div>
 
-        <div className="mt-20 h-fit">
+        <div className="mt-20 h-fit mx-auto w-[1000px]">
           <div className="flex justify-between w-full">
             <div className="flex justify-between">
               <div className="mt-0.5 w-[40%] flex">
@@ -99,55 +103,63 @@ export function LatarBelakang() {
           </div>
         </div>
 
-        <div className="h-fit">
-          <div className="flex justify-between w-full">
-            <div className="flex flex-row-reverse justify-between">
-              <div className="mt-0.5 w-[40%] flex">
-                <Image src={Edit} alt="Edit SVG" className="w-12 h-12" />
-                <div className="ml-5">
-                  <p className="-mb-1">02</p>
-                  <p>Mulai Asesmen</p>
+        <div className="h-fit relative">
+          <Image
+            src={Effect01}
+            alt="Effect Image"
+            className="absolute top-1 z-20 -right-1"
+          />
+          <div className="w-[1000px] mx-auto">
+            <div className="flex justify-between w-full">
+              <div className="flex flex-row-reverse justify-between">
+                <div className="mt-0.5 w-[40%] flex">
+                  <Image src={Edit} alt="Edit SVG" className="w-12 h-12" />
+                  <div className="ml-5">
+                    <p className="-mb-1">02</p>
+                    <p>Mulai Asesmen</p>
 
-                  <p className="mt-5">
-                    Ukur seberapa jauh kemampuanmu dalam suatu materi matematika
-                    melalui asesmen hingga pada materi prasyarat nya!
-                  </p>
+                    <p className="mt-5">
+                      Ukur seberapa jauh kemampuanmu dalam suatu materi
+                      matematika melalui asesmen hingga pada materi prasyarat
+                      nya!
+                    </p>
 
-                  <p className="my-5">Pada asesmen terdiri atas:</p>
+                    <p className="my-5">Pada asesmen terdiri atas:</p>
 
-                  <div className="flex">
-                    <Image
-                      src={GreenCheck}
-                      alt="Green Check Svg"
-                      className="mr-3"
-                    />
-                    <p>Fitur Tracking</p>
+                    <div className="flex">
+                      <Image
+                        src={GreenCheck}
+                        alt="Green Check Svg"
+                        className="mr-3"
+                      />
+                      <p>Fitur Tracking</p>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="relative -mt-7">
-                <Image
-                  src={Circle}
-                  alt="Circle Effect"
-                  className="w-[80%] h-[80%] object-contain ml-10 -mt-8"
-                />
-                <Image
-                  src={SecondMain}
-                  alt="Circle Effect"
-                  className="absolute top-14"
-                />
-                <Image
-                  src={SecondBottom}
-                  alt="Circle Effect"
-                  className="absolute bottom-52 -left-10 z-10 w-[45%] h-[45%] object-contain"
-                />
+                <div className="relative -mt-7">
+                  <Image
+                    src={Circle}
+                    alt="Circle Effect"
+                    className="w-[80%] h-[80%] object-contain ml-10 -mt-8"
+                  />
+                  <Image
+                    src={SecondMain}
+                    alt="Circle Effect"
+                    className="absolute top-14"
+                  />
+                  <Image
+                    src={SecondBottom}
+                    alt="Circle Effect"
+                    className="absolute bottom-52 -left-10 z-10 w-[45%] h-[45%] object-contain"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="h-fit">
+        <div className="h-fit mx-auto w-[1000px]">
           <div className="flex justify-between w-full">
             <div className="flex justify-between">
               <div className="mt-0.5 w-[40%] flex">
