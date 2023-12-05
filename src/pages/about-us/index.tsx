cd33694b3e9ab7f005c2ca367b688e2990e07936
Wahyudi Umar <wahyucherryorlove@gmail.com>
2023-12-05 18:10:15 +0700

    fix: perbaiki halaman about me and roadmap

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -40,98 +40,100 @@ export default function AboutUs() {
         <meta property="twitter:domain" content="emteka.id" />
       </Head>
 
-      <div className="relative overflow-x-clip">
-        <div className="w-[477px] h-[477px] absolute -top-60 -right-60 opacity-50 bg-[#FFE600] rounded-full blur-[200px]" />
-      </div>
-
-      <WhatsappButton />
-
-      <div className="relative flex flex-col items-center justify-center max-h-fit mt-32 xl:mt-16 px-4 xl:px-0 pt-[77px] font-karla">
-        <Image priority src={HeroImage} alt="" className="w-[790px]" />
-        <Image
-          priority
-          src={LogoFuraha}
-          alt=""
-          className="w-28 absolute top-0 xl:relative xl:-top-72"
-        />
-        <Image
-          priority
-          src={Title}
-          alt=""
-          className="w-4/5 xl:w-[503px] relative mt-5 xl:mt-0 bottom-0 xl:bottom-20 drop-shadow-md"
-        />
-        <p className="text-center max-w-xl xl:-mt-16 text-neutral-0">
-          Ini dimulai dari semangat tim Furaha Systems untuk mendukung kemajuan
-          dan pengembangan SDM Indonesia melalui Inovasi.
-        </p>
-      </div>
-
-      <Image className="w-0.5 xl:w-1 my-6 mx-auto" src={Divider} alt={""} />
-
-      <div className="flex flex-col mx-auto gap-4 w-full xl:max-w-3xl px-4 xl:px-0 text-neutral-0 font-karla">
-        <h4 className="text-2xl font-bold text-center">
-          Didirikannya Emteka juga didasari oleh adanya{" "}
-          <span className="text-primary-400">
-            kesenjangan signifikan dalam pengetahuan dan keterampilan Matematika
-            siswa/i Indonesia.
-          </span>
-        </h4>
-        <p className="text-xl font-normal text-center">
-          Oleh karena itu, Furaha Systems menghadirkan Emteka untuk membantu
-          mereka mencapai potensi terbaik dalam belajar Matematika!
-        </p>
-      </div>
-
-      <Image className="w-0.5 xl:w-1 my-6 mx-auto" src={Divider} alt={""} />
-
-      <div className="flex flex-col lg:flex-row gap-16 justify-center text-neutral-0 font-karla px-4 xl:px-0 items-center xl:items-start">
-        <Image priority src={HeroImage2} alt="" className="w-[400px]" />
-        <div className="flex flex-col items-center text-center xl:text-start xl:items-start">
-          <Image priority src={LogoFuraha} alt="" className="w-28" />
-          <p className="mt-4 font-normal text-xl max-w-lg">
-            <span className="text-primary-400 font-bold">Furaha Systems</span>{" "}
-            adalah sebuah organisasi tim pengembangan startup yang dibangun atas
-            dasar keprihatinan terhadap sistem pengembangan SDM khususnya di
-            Indonesia dan kepedulian akan percepatan implementasi teknologi
-            perangkat lunak secara komprehensif sebagai upaya dalam mendukung
-            pertumbuhan ekonomi di Indonesia.
-          </p>
-          <a
-            href="https://www.furahasystems.com/"
-            target="_blank"
-            className="bg-primary-400 rounded-full w-max py-3 px-8 mt-6 text-sm font-bold text-neutral-900 "
-          >
-            Cari tahu lebih lanjut
-          </a>
+      <div className="bg-secondary-500 pt-[7rem] pb-10 xl:pt-0">
+        <div className="relative overflow-x-clip">
+          <div className="w-[477px] h-[477px] absolute -top-60 -right-60 opacity-50 bg-[#FFE600] rounded-full blur-[200px]" />
         </div>
-      </div>
 
-      <Image className="w-0.5 xl:w-1 my-6 mx-auto" src={Divider} alt={""} />
+        <WhatsappButton />
 
-      <div className="flex flex-col gap-8 items-center font-karla mb-[104px]">
-        <h1 className="text-4xl xl:text-5xl font-bold text-neutral-0">
-          Penghargaan
-        </h1>
-        <div className="flex flex-col xl:flex-row gap-6">
-          <div className="flex flex-col items-center justify-center py-8 px-4 gap-6 h-[199px] w-full xl:w-[357px] rounded-2xl bg-secondary-500 border-2 border-[#F2F6FF40]">
-            <Image
-              src={Penghargaan1}
-              alt={""}
-              className="w-[203px] mix-blend-plus-lighter	"
-            />
-            <p className="text-base font-bold text-center text-primary-300 w-full">
-              Kota Tangerang Selatan. Tahun 2023
+        <div className="relative flex flex-col items-center justify-center max-h-fit xl:mt-16 px-4 xl:px-0 pt-20 xl:pt-[77px] font-karla">
+          <Image priority src={HeroImage} alt="" className="w-[790px]" />
+          <Image
+            priority
+            src={LogoFuraha}
+            alt=""
+            className="w-28 absolute top-0 xl:relative xl:-top-72"
+          />
+          <Image
+            priority
+            src={Title}
+            alt=""
+            className="w-4/5 xl:w-[503px] relative mt-5 xl:mt-0 bottom-0 xl:bottom-20 drop-shadow-md"
+          />
+          <p className="text-center max-w-xl xl:-mt-16 text-neutral-0">
+            Ini dimulai dari semangat tim Furaha Systems untuk mendukung
+            kemajuan dan pengembangan SDM Indonesia melalui Inovasi.
+          </p>
+        </div>
+
+        <Image className="w-1 my-6 mx-auto" src={Divider} alt={""} />
+
+        <div className="flex flex-col mx-auto gap-4 w-full xl:max-w-3xl px-4 xl:px-0 text-neutral-0 font-karla">
+          <h4 className="text-2xl font-bold text-center">
+            Didirikannya Emteka juga didasari oleh adanya{" "}
+            <span className="text-primary-400">
+              kesenjangan signifikan dalam pengetahuan dan keterampilan
+              Matematika siswa/i Indonesia.
+            </span>
+          </h4>
+          <p className="text-xl font-normal text-center">
+            Oleh karena itu, Furaha Systems menghadirkan Emteka untuk membantu
+            mereka mencapai potensi terbaik dalam belajar Matematika!
+          </p>
+        </div>
+
+        <Image className="w-1 my-6 mx-auto" src={Divider} alt={""} />
+
+        <div className="flex flex-col lg:flex-row gap-16 justify-center text-neutral-0 font-karla px-4 xl:px-0 items-center xl:items-start">
+          <Image priority src={HeroImage2} alt="" className="w-[400px]" />
+          <div className="flex flex-col items-center text-center xl:text-start xl:items-start">
+            <Image priority src={LogoFuraha} alt="" className="w-28" />
+            <p className="mt-4 font-normal text-xl max-w-lg">
+              <span className="text-primary-400 font-bold">Furaha Systems</span>{" "}
+              adalah sebuah organisasi tim pengembangan startup yang dibangun
+              atas dasar keprihatinan terhadap sistem pengembangan SDM khususnya
+              di Indonesia dan kepedulian akan percepatan implementasi teknologi
+              perangkat lunak secara komprehensif sebagai upaya dalam mendukung
+              pertumbuhan ekonomi di Indonesia.
             </p>
+            <a
+              href="https://www.furahasystems.com/"
+              target="_blank"
+              className="bg-primary-400 rounded-full w-max py-3 px-8 mt-6 text-sm font-bold text-neutral-900 "
+            >
+              Cari tahu lebih lanjut
+            </a>
           </div>
-          <div className="flex flex-col items-center justify-center py-8 px-4 gap-6 h-[199px] w-full xl:w-[357px] rounded-2xl bg-secondary-500 border-2 border-[#F2F6FF40]">
-            <Image
-              src={Penghargaan2}
-              alt={""}
-              className="w-[273px] mix-blend-plus-lighter	"
-            />
-            <p className="text-base font-bold text-center text-primary-300 w-full">
-              Kota Tangerang Selatan. Tahun 2023
-            </p>
+        </div>
+
+        <Image className="w-1 my-6 mx-auto" src={Divider} alt={""} />
+
+        <div className="flex flex-col gap-8 items-center font-karla mb-[104px]">
+          <h1 className="text-4xl xl:text-5xl font-bold text-neutral-0">
+            Penghargaan
+          </h1>
+          <div className="flex flex-col xl:flex-row gap-6">
+            <div className="flex flex-col items-center justify-center py-8 px-4 gap-6 h-[199px] w-full xl:w-[357px] rounded-2xl bg-secondary-500 border-2 border-[#F2F6FF40]">
+              <Image
+                src={Penghargaan1}
+                alt={""}
+                className="w-[203px] mix-blend-plus-lighter	"
+              />
+              <p className="text-base font-bold text-center text-primary-300 w-full">
+                Kota Tangerang Selatan. Tahun 2023
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center py-8 px-4 gap-6 h-[199px] w-full xl:w-[357px] rounded-2xl bg-secondary-500 border-2 border-[#F2F6FF40]">
+              <Image
+                src={Penghargaan2}
+                alt={""}
+                className="w-[273px] mix-blend-plus-lighter	"
+              />
+              <p className="text-base font-bold text-center text-primary-300 w-full">
+                Kota Tangerang Selatan. Tahun 2023
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/roadmap/index.tsx
+++ src/pages/roadmap/index.tsx
@@ -44,58 +44,60 @@ export default function RoadMapPage() {
         <meta property="twitter:domain" content="emteka.id" />
       </Head>
 
-      <div className="relative overflow-x-clip flex flex-col justify-center items-center">
-        <div className="w-[477px] h-[477px] absolute -top-[14rem] opacity-50 bg-[#FFE600] rounded-full blur-[200px] -z-10" />
+      <div className="bg-secondary-500">
+        <div className="relative overflow-x-clip flex flex-col justify-center items-center z-10">
+          <div className="w-[477px] h-[477px] absolute -top-[14rem] opacity-50 bg-[#FFE600] rounded-full blur-[200px] -z-10" />
+        </div>
+        <main className="relative z-10 mx-auto w-full xl:w-3/4 font-karla px-4 xl:px-0 pb-10 pt-[150px]">
+          <section className="flex flex-col gap-y-4 text-center mx-auto xl:w-[738px] mb-8 px-4 xl:px-0">
+            <h1 className="text-5xl font-semibold text-neutral-0">
+              Ikuti Jalan Produk Ini
+            </h1>
+            <p className="text-neutral-0 text-lg">
+              Kami akan menciptakan lingkungan pembelajaran matematika yang
+              lebih dinamis, responsif, dan menantang, membantu pengguna
+              mencapai tingkat penguasaan yang lebih tinggi dan memperoleh
+              keterampilan matematika yang lebih mendalam.
+            </p>
+          </section>
+
+          {width > 1200 && <CardDesktop />}
+
+          {width < 680 &&
+            CardTemp.map(({ id, title, paragraph, images }) => (
+              <CardMobile
+                key={id}
+                id={id}
+                title={title}
+                paragraph={paragraph}
+                images={images}
+              />
+            ))}
+
+          <Image className="w-1 my-6 mx-auto" src={Divider} alt={""} />
+
+          <section className="flex flex-col justify-center items-center text-center gap-y-4 mb-[120px]">
+            <h2 className="text-5xl font-semibold text-neutral-0">
+              Kolaborasi Industri
+            </h2>
+            <p className="xl:w-[738px] text-lg font-normal text-neutral-0">
+              Menjalin kemitraan dengan perusahaan terkemuka untuk memastikan
+              materi pembelajaran sesuai dengan kebutuhan industri, sehingga
+              lulusan memiliki keterampilan yang sesuai dengan tuntutan pasar
+              kerja.
+            </p>
+
+            <Link
+              href="mailto:info@emteka.id"
+              className="bg-primary-400 px-8 font-semibold py-[10px] rounded-full text-sm mt-4 xl:mt-0"
+            >
+              Tertarik untuk bekerja sama?
+            </Link>
+          </section>
+        </main>
+
+        <WhatsappButton />
       </div>
-      <main className="y-[75px] mx-auto w-full xl:w-3/4 font-karla px-4 xl:px-0">
-        <section className="flex flex-col gap-y-4 text-center mt-[150px] mx-auto xl:w-[738px] mb-8 px-4 xl:px-0">
-          <h1 className="text-5xl font-semibold text-neutral-0">
-            Ikuti Jalan Produk Ini
-          </h1>
-          <p className="text-neutral-0 text-lg">
-            Kami akan menciptakan lingkungan pembelajaran matematika yang lebih
-            dinamis, responsif, dan menantang, membantu pengguna mencapai
-            tingkat penguasaan yang lebih tinggi dan memperoleh keterampilan
-            matematika yang lebih mendalam.
-          </p>
-        </section>
-
-        {width > 1200 && <CardDesktop />}
-
-        {width < 680 &&
-          CardTemp.map(({ id, title, paragraph, images }) => (
-            <CardMobile
-              key={id}
-              id={id}
-              title={title}
-              paragraph={paragraph}
-              images={images}
-            />
-          ))}
-
-        <Image className="w-1 my-6 mx-auto" src={Divider} alt={""} />
-
-        <section className="flex flex-col justify-center items-center text-center gap-y-4 mb-[120px]">
-          <h2 className="text-5xl font-semibold text-neutral-0">
-            Kolaborasi Industri
-          </h2>
-          <p className="xl:w-[738px] text-lg font-normal text-neutral-0">
-            Menjalin kemitraan dengan perusahaan terkemuka untuk memastikan
-            materi pembelajaran sesuai dengan kebutuhan industri, sehingga
-            lulusan memiliki keterampilan yang sesuai dengan tuntutan pasar
-            kerja.
-          </p>
-
-          <Link
-            href="mailto:info@emteka.id"
-            className="bg-primary-400 px-8 font-semibold py-[10px] rounded-full text-sm mt-4 xl:mt-0"
-          >
-            Tertarik untuk bekerja sama?
-          </Link>
-        </section>
-      </main>
-
-      <WhatsappButton />
     </>
   );
 }
